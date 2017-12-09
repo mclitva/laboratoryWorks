a0a6485ae6a3360a1912aedb20b1c7075718e3cf
Dmitry <Dmitry.Litovchenko1@yandex.ru>
2017-12-09 18:51:07 +0300

    Missed class Identifier

--- kNNImplementation.py
+++ kNNImplementation.py
@@ -1,6 +1,6 @@
 """
-This example show how to classify wine
-based on an existing dataset from UCI Machine Learning Repository.
+This eiample show how to classify wine
+based on an eiisting dataset from UCI Machine Learning Repository.
 The classification is made according to seven neighbors.
 """
 import random
@@ -17,38 +17,38 @@ def loadDataset(url):
 
 
 def GetTestAndTrainingSets(data_set, split_coef, test_set = [], training_set = []):
-    for x in range(len(data_set) - 1):
+    for i in range(len(data_set) - 1):
         if random.random() >= split_coef:
-            test_set.append(data_set[x, :])
+            test_set.append(data_set[i, :])
         else:
-            training_set.append(data_set[x, :])
+            training_set.append(data_set[i, :])
 
 
 
 def getEuclideanDistance(instance_to, instance_from, length):
     distance = 0
-    for x in range(length):
-        distance += pow((instance_to[x] - instance_from[x]), 2)
+    for i in range(length):
+        distance += pow((instance_to[i] - instance_from[i]), 2)
     return math.sqrt(distance)
 
 
 def getNeighbors(training_set, test_set_instance, k):
     distances = []
     length = len(test_set_instance) - 1
-    for x in range(len(training_set)):
-        dist = getEuclideanDistance(test_set_instance, training_set[x], length)
-        distances.append((training_set[x], dist))
+    for i in range(len(training_set)):
+        dist = getEuclideanDistance(test_set_instance, training_set[i], length)
+        distances.append((training_set[i], dist))
     distances.sort(key=operator.itemgetter(1))
     neighbors = []
-    for x in range(k):
-        neighbors.append(distances[x][0])      
+    for i in range(k):
+        neighbors.append(distances[i][0])      
     return neighbors
 
 
 def getClassVoteResult(neighbors):
     class_votes = {}
-    for x in range(len(neighbors)):
-        response = neighbors[x][-1]
+    for i in range(len(neighbors)):
+        response = neighbors[i][0]
         if response in class_votes:
             class_votes[response] += 1
         else:
@@ -60,8 +60,8 @@ def getClassVoteResult(neighbors):
 
 def calculateAccuracy(test_set, predicted_values):
     correct = 0
-    for x in range(len(test_set)):
-        if test_set[x][-1] == predicted_values[x]:
+    for i in range(len(test_set)):
+        if test_set[i][0] == predicted_values[i]:
             correct += 1
     return (correct / float(len(test_set))) * 100.0
 
@@ -69,7 +69,7 @@ def calculateAccuracy(test_set, predicted_values):
 def main():
     url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
     split_coef = 0.67
-    k = 7
+    k = 5
     data_set = loadDataset(url)
     training_set = []
     test_set = []
@@ -79,11 +79,11 @@ def main():
     print("Test set length: " + repr(len(test_set)))
     predicted_values = []
     print("Input new data from the test set with count of neighbors = %i:" % k)
-    for x in range(len(test_set)):
-        neighbors = getNeighbors(training_set, test_set[x], k)
+    for i in range(len(test_set)):
+        neighbors = getNeighbors(training_set, test_set[i], k)
         vote_result = getClassVoteResult(neighbors)
         predicted_values.append(vote_result)        
-        print("> Predicted value=%.2f, actual=%.2f" % (vote_result, test_set[x][-1]))
+        print("> Predicted value=%.2f, actual=%.2f" % (vote_result, test_set[i][0]))
     accuracy = calculateAccuracy(test_set, predicted_values)
     print("Accuracy: " + repr(round(accuracy,4)) + '%')
 
