eb207c9e59c35c0158f546c6f68ea2b5fea1df8c
mclitva <ss@ss.ru>
2018-01-12 22:24:55 +0300

    Linear Ok

--- Linear/LinearRegression.py
+++ Linear/LinearRegression.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from math import pow
-from sklearn.datasets import load_boston 
+import matplotlib.pyplot as plot
+from sklearn.datasets import load_boston
  
 def get_headers(dataframe):
     return dataframe.columns.values
@@ -9,18 +10,18 @@ def get_headers(dataframe):
 def average(readings):
     readings_total = sum(readings)
     number_of_readings = len(readings)
-    mean = readings_total / float(number_of_readings)
-    return mean
+    average = readings_total / float(number_of_readings)
+    return average
  
  
-def cal_variance(readings):
+def varianceOf(readings):
     readings_average = average(readings)
     mean_difference_squared_readings = [pow((reading - readings_average), 2) for reading in readings]
     variance = sum(mean_difference_squared_readings)
     return variance / float(len(readings) - 1)
  
  
-def cal_covariance(readings_1, readings_2):
+def covarianceBetween(readings_1, readings_2):
     readings_1_average = average(readings_1)
     readings_2_average = average(readings_2)
     readings_size = len(readings_1)
@@ -30,8 +31,8 @@ def cal_covariance(readings_1, readings_2):
     return covariance / float(readings_size - 1)
  
  
-def cal_simple_linear_regression_coefficients(x_readings, y_readings):
-    b1 = cal_covariance(x_readings, y_readings) / float(cal_variance(x_readings))
+def simpleLinearRegressionCoefficients(x_readings, y_readings):
+    b1 = covarianceBetween(x_readings, y_readings) / float(varianceOf(x_readings))
     b0 = average(y_readings) - (b1 * average(x_readings))
     return b0, b1
  
@@ -40,10 +41,10 @@ def predict_target_value(x, b0, b1):
     return b0 + b1 * x
  
  
-def cal_rmse(actual_readings, predicted_readings):
+def rootMeanSquareError(actual_readings, predicted_readings):
     square_error_total = 0.0
     total_readings = len(actual_readings)
-    for i in xrange(0, total_readings):
+    for i in range(0, total_readings):
         error = predicted_readings[i] - actual_readings[i]
         square_error_total += pow(error, 2)
     rmse = square_error_total / float(total_readings)
@@ -56,24 +57,36 @@ def simple_linear_regression(dataset):
     print("Dataset Headers :: ", (dataset_headers))
  
     # Calculating the mean of the square feet and the price readings
-    square_feet_mean = average(dataset[dataset_headers[0]])
-    price_mean = average(dataset[dataset_headers[1]])
+    square_feet_average = average(dataset[dataset_headers[0]])
+    price_average = average(dataset[dataset_headers[1]])
  
-    square_feet_variance = cal_variance(dataset[dataset_headers[0]])
-    price_variance = cal_variance(dataset[dataset_headers[1]])
+    square_feet_variance = varianceOf(dataset[dataset_headers[0]])
+    price_variance = varianceOf(dataset[dataset_headers[1]])
  
     # Calculating the regression
     covariance_of_price_and_square_feet = dataset.cov()[dataset_headers[0]][dataset_headers[1]]
     w1 = covariance_of_price_and_square_feet / float(square_feet_variance)
  
-    w0 = price_mean - (w1 * square_feet_mean)
+    w0 = price_average - (w1 * square_feet_average)
  
     # Predictions
-    dataset['Predicted_Price'] = w0 + w1 * dataset[dataset_headers[0]]
+    dataset['PredictedPrice'] = w0 + w1 * dataset[dataset_headers[0]]
+    print(dataset[['PredictedPrice','Size']])
+    print(rootMeanSquareError(dataset[dataset_headers[1]],dataset['PredictedPrice']))
+    return dataset
+    
+
  
  
 if __name__ == "__main__":
- 
-    input_path = '../Inputs/input_data.csv'
+    #dataset downloaded from https://wiki.csc.calpoly.edu/datasets/wiki/Houses
+    input_path = 'RealEstate.csv'
     house_price_dataset = pd.read_csv(input_path)
-    simple_linear_regression(house_price_dataset)
+    price_footage_dataset = house_price_dataset[['Size', 'Price']]
+    calculatedDataset = simple_linear_regression(price_footage_dataset)
+    plot.scatter(calculatedDataset.Price, calculatedDataset.Size, c = 'b', s = 20 , alpha = 0.5)
+    plot.scatter(calculatedDataset.PredictedPrice, calculatedDataset.Size, c = 'r', s = 15)
+    plot.title('Distribution of prices for houses depending on the size')
+    plot.ylabel('Size in Foots')
+    plot.xlabel('Price')
+    plot.show()
